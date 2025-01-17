b8df198879d35828d876f37f173f5df5fce605eb
🤖 automated <cron@job>
2025-01-17 10:55:48 +0100

    MacBook Pro Tim (2)

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,12 +1,19 @@
 return {
-	"preservim/vim-markdown",
-	ft = { "markdown", "codecompanion" },
-	init = function()
-		vim.cmd [[set foldlevelstart=6]]
-		vim.cmd [[set conceallevel=2]]
-		vim.cmd [[let g:vim_markdown_auto_insert_bullets = 0]]
-	end,
+	"OXY2DEV/markview.nvim",
+	lazy = false, -- Recommended
+	ft = { "markdown", "codecompanion" }, -- If you decide to lazy-load anyway
+
 	dependencies = {
-		"godlygeek/tabular",
-	},
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+	}, -- "preservim/vim-markdown",
+	-- ft = { "markdown", "codecompanion" },
+	-- init = function()
+	-- 	vim.cmd [[set foldlevelstart=6]]
+	-- 	vim.cmd [[set conceallevel=2]]
+	-- 	vim.cmd [[let g:vim_markdown_auto_insert_bullets = 0]]
+	-- end,
+	-- dependencies = {
+	-- 	"godlygeek/tabular",
+	-- },
 }
