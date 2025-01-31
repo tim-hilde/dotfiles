736f1fd40bd0cc735abb793f5430c741ba0c3e48
🤖 automated <cron@job>
2025-01-31 14:16:20 +0100

    MacBook Pro Tim (1)

--- nvim/lua/plugins/git-conflict.lua
+++ nvim/lua/plugins/git-conflict.lua
@@ -1,5 +1,14 @@
 return {
 	"akinsho/git-conflict.nvim",
 	version = "*",
-	config = true,
+	opts = {
+		default_mappings = {
+			ours = "o",
+			theirs = "t",
+			none = "0",
+			both = "b",
+			next = "n",
+			prev = "p",
+		},
+	},
 }
