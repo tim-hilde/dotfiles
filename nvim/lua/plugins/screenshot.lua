91680807467adaf7185cd3ba35089cd72cabd411
🤖 automated <cron@job>
2025-02-13 15:32:45 +0100

    MacBook Pro Tim (1)

--- nvim/lua/plugins/screenshot.lua
+++ nvim/lua/plugins/screenshot.lua
@@ -13,9 +13,7 @@ return {
 			line_offset = function(args)
 				return args.line1
 			end,
-			-- language = function()
-			-- 	return vim.bo.filetype
-			-- end,
+			num_separator = "  ",
 			window_title = function()
 				return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
 			end,
