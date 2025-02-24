9c76f09f650d77824af5f0d48dab5979bc25d8e7
🤖 automated <cron@job>
2025-02-24 14:25:48 +0100

    MacBook Pro Tim (2)

--- nvim/lua/plugins/linting.lua
+++ nvim/lua/plugins/linting.lua
@@ -8,6 +8,7 @@ return {
 			lint.linters_by_ft = {
 				yaml = { "yamllint" },
 				shell = { "shellcheck" },
+				python = { "mypy" },
 			}
 
 			-- To allow other plugins to add linters to require('lint').linters_by_ft,
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -91,7 +91,7 @@ return {
 						end
 
 						-- Set the border style for the hover and signature help windows
-						vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(hover, { border = "rounded" })
+						vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 						vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
 
 						-- Jump to the definition of the word under your cursor.
