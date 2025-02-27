206d3f8096d491aa4168caa6015f5e32d4c42401
🤖 automated <cron@job>
2025-02-27 12:13:09 +0100

    MacBook Pro Tim (1)

--- nvim/lua/plugins/lsp_signature.lua
+++ nvim/lua/plugins/lsp_signature.lua
@@ -1,15 +1,20 @@
 return {
 	"ray-x/lsp_signature.nvim",
 	event = "InsertEnter",
-	opts = {
-		bind = true,
-		handler_opts = {
-			border = "rounded",
-		},
-		hint_enable = true,
-		floating_window = false,
-		hint_prefix = {
-			above = "↙ ", -- when the hint is on the line above the current line
-		},
-	},
+	opts = function()
+		require("lsp_signature").on_attach {
+			bind = true,
+			handler_opts = {
+				border = "rounded",
+			},
+			hint_enable = true,
+			floating_window = false,
+			toggle_key_flip_floatwin_setting = true,
+			hint_prefix = {
+				above = "↙ ", -- when the hint is on the line above the current line
+				current = "← ", -- when the hint is on the same line
+				below = "↖ ", -- when the hint is on the line below the current line
+			},
+		}
+	end,
 }
