72a83ea0cc66666cfee43220d831312cc68f006e
🤖 automated <cron@job>
2025-03-13 09:07:32 +0100

    MacBook Pro Tim (1)

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -9,17 +9,17 @@ return {
 	config = function()
 		require("neo-tree").setup {
 			close_if_last_window = true,
-			filesystem = {
-				filtered_items = {
-					visible = true, -- when true, they will just be displayed differently than normal items
-					hide_dotfiles = false,
-					hide_gitignored = false,
-					never_show = {
-						".DS_Store",
-						"thumbs.db",
-					},
-				},
-			},
+			-- filesystem = {
+			-- 	filtered_items = {
+			-- 		visible = true, -- when true, they will just be displayed differently than normal items
+			-- 		hide_dotfiles = false,
+			-- 		hide_gitignored = false,
+			-- 		never_show = {
+			-- 			".DS_Store",
+			-- 			"thumbs.db",
+			-- 		},
+			-- 	},
+			-- },
 		}
 	end,
 }
