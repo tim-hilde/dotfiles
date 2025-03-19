46db21705a21a5fa300c0d46dfe685cc9dc304d3
🤖 automated <cron@job>
2025-03-19 16:03:07 +0100

    MacBook Pro Tim (1)

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -184,6 +184,10 @@ return {
 						--  This is where a variable was first declared, or where a function is defined, etc.
 						--  To jump back, press <C-t>.
 						map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+						map("g<c-d>", function()
+							vim.cmd "vsplit"
+							require("telescope.builtin").lsp_definitions()
+						end, "[G]oto [D]efinition (split)")
 
 						-- Find references for the word under your cursor.
 						map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
