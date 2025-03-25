a4c2f03f85fa9258b0186aceed8d085f4c23d54a
🤖 automated <cron@job>
2025-03-25 12:59:25 +0100

    MacBook Tim (6)

--- nvim/lua/plugins/code-companion.lua
+++ nvim/lua/plugins/code-companion.lua
@@ -79,6 +79,15 @@ return {
 			strategies = {
 				chat = {
 					adapter = "copilot",
+					slash_commands = {
+						codebase = require("vectorcode.integrations").codecompanion.chat.make_slash_command(),
+					},
+					tools = {
+						vectorcode = {
+							description = "Run VectorCode to retrieve the project context.",
+							callback = require("vectorcode.integrations").codecompanion.chat.make_tool(),
+						},
+					},
 				},
 				inline = {
 					adapter = "copilot",
