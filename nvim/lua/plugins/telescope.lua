12b828c7776eec766397e4258759ea279359b4ea
🤖 automated <cron@job>
2025-03-31 15:56:45 +0200

    MacBook Pro Tim (1)

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -16,6 +16,8 @@ return {
 
 			-- Useful for getting pretty icons, but requires a Nerd Font.
 			{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+
+			"nvim-telescope/telescope-media-files.nvim",
 		},
 		config = function()
 			-- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -65,6 +67,7 @@ return {
 			-- Enable Telescope extensions if they are installed
 			pcall(require("telescope").load_extension, "fzf")
 			pcall(require("telescope").load_extension, "ui-select")
+			pcall(require("telescope").load_extension, "media_files")
 
 			-- See `:help telescope.builtin`
 			local builtin = require "telescope.builtin"
