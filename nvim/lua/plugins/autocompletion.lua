6c546a8ce407ee7d4e4f569522045605f94ebbd7
🤖 automated <cron@job>
2025-04-04 19:32:38 +0200

    MacBook Pro Tim (2)

--- nvim/lua/plugins/autocompletion.lua
+++ nvim/lua/plugins/autocompletion.lua
@@ -1,85 +1,99 @@
 return {
-	"saghen/blink.cmp",
-	-- optional: provides snippets for the snippet source
-	dependencies = {
-		"rafamadriz/friendly-snippets",
-		"Kaiser-Yang/blink-cmp-git",
-		"L3MON4D3/LuaSnip",
+	{
+		"xzbdmw/colorful-menu.nvim",
+		opts = true,
 	},
-
-	version = "*",
-	-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-	-- build = "cargo build --release",
-	-- If you use nix, you can build from source using latest nightly rust with:
-	-- build = 'nix run .#build-plugin',
-
-	---@module 'blink.cmp'
-	---@type blink.cmp.Config
-	opts = {
-		keymap = { preset = "default" },
-
-		appearance = {
-			nerd_font_variant = "mono",
+	{
+		"saghen/blink.cmp",
+		-- optional: provides snippets for the snippet source
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			"Kaiser-Yang/blink-cmp-git",
+			"L3MON4D3/LuaSnip",
 		},
 
-		completion = {
-			menu = {
-				border = "rounded",
-				winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine: BlinkCmpMenuSelection,Search:None",
-				draw = {
-					gap = 2,
-					columns = {
-						{
-							"label",
-							"label_description",
-							gap = 1,
-						},
-						{
-							"kind",
-							gap = 1,
-							"kind_icon",
+		version = "*",
+		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+		-- build = "cargo build --release",
+		-- If you use nix, you can build from source using latest nightly rust with:
+		-- build = 'nix run .#build-plugin',
+
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			keymap = { preset = "default" },
+
+			appearance = {
+				nerd_font_variant = "mono",
+			},
+
+			completion = {
+				menu = {
+					border = "rounded",
+					winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpMenuSelection,Search:None",
+					draw = {
+						gap = 2,
+						columns = { { "kind_icon" }, { "label", gap = 1 } },
+						components = {
+							label = {
+								text = function(ctx)
+									return require("colorful-menu").blink_components_text(ctx)
+								end,
+								highlight = function(ctx)
+									local highlights = {}
+									local highlights_info = require("colorful-menu").blink_highlights(ctx)
+									if highlights_info ~= nil then
+										highlights = highlights_info.highlights
+									end
+									for _, idx in ipairs(ctx.label_matched_indices) do
+										table.insert(highlights, { idx, idx + 1, group = "BlinkCmpLabelMatch" })
+									end
+									-- Do something else
+									return highlights
+								end,
+							},
 						},
 					},
 				},
+				documentation = {
+					auto_show = true,
+					window = {
+						border = "rounded",
+						winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
+					},
+				},
 			},
-			documentation = {
-				auto_show = true,
+			signature = {
 				window = {
 					border = "rounded",
-					winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
 				},
 			},
-		},
-		signature = {
-			window = {
-				-- border = "rounded",
+			snippets = {
+				preset = "default",
 			},
-		},
-		snippets = {
-			preset = "default",
-		},
-		sources = {
-			default = { "lsp", "path", "snippets", "buffer", "git" },
-			per_filetype = {
-				codecompanion = { "codecompanion" },
-			},
-			providers = {
-				git = {
-					module = "blink-cmp-git",
-					name = "Git",
-					opts = {
-						-- options for the blink-cmp-git
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer", "git" },
+				per_filetype = {
+					codecompanion = { "codecompanion" },
+				},
+				providers = {
+					git = {
+						module = "blink-cmp-git",
+						name = "Git",
+						opts = {
+							-- options for the blink-cmp-git
+						},
 					},
 				},
 			},
-		},
 
-		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-		--
-		-- See the fuzzy documentation for more information
-		fuzzy = { implementation = "prefer_rust_with_warning" },
+			-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+			-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+			-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+			--
+			-- See the fuzzy documentation for more information
+			fuzzy = { implementation = "prefer_rust_with_warning" },
+		},
+		opts_extend = { "sources.default" },
 	},
-	opts_extend = { "sources.default" },
 }
