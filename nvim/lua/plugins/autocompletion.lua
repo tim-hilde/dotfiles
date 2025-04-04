6233bcf8b61c494c0af496ed9c44aa4d011e8d6c
🤖 automated <cron@job>
2025-04-04 19:47:54 +0200

    MacBook Pro Tim (1)

--- nvim/lua/plugins/autocompletion.lua
+++ nvim/lua/plugins/autocompletion.lua
@@ -40,16 +40,7 @@ return {
 									return require("colorful-menu").blink_components_text(ctx)
 								end,
 								highlight = function(ctx)
-									local highlights = {}
-									local highlights_info = require("colorful-menu").blink_highlights(ctx)
-									if highlights_info ~= nil then
-										highlights = highlights_info.highlights
-									end
-									for _, idx in ipairs(ctx.label_matched_indices) do
-										table.insert(highlights, { idx, idx + 1, group = "BlinkCmpLabelMatch" })
-									end
-									-- Do something else
-									return highlights
+									return require("colorful-menu").blink_components_highlight(ctx)
 								end,
 							},
 						},
