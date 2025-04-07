6dd72ba7706db2f4024108acbc3a2ac26686691a
🤖 automated <cron@job>
2025-04-07 09:55:12 +0200

    MacBook Pro Tim (1)

--- yazi/init.lua
+++ yazi/init.lua
@@ -11,3 +11,7 @@ function Linemode:size_and_mtime()
 	local size = self._file:size()
 	return ui.Line(string.format("%s %s", size and ya.readable_size(size) or "-", time))
 end
+
+require("session"):setup({
+	sync_yanked = true,
+})
