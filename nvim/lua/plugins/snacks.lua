a34a3cef45c44e0b6d1a84e2159b3c67975e47a2
🤖 automated <cron@job>
2025-04-11 08:16:51 +0200

    MacBook Pro Tim (1)

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -16,5 +16,45 @@ return {
 		picker = {
 			enabled = true,
 		},
+		keys = {
+
+			{
+				"n",
+				"<leader>sh",
+				function()
+					Snacks.picker.help()
+				end,
+				{ desc = "[S]earch [H]elp" },
+			},
+			-- {"n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" }},
+			{
+				"n",
+				"<leader>sf",
+				function()
+					Snacks.picker.smart()
+				end,
+				{ desc = "[S]earch [F]iles" },
+			},
+			{
+				"n",
+				"<leader>ss",
+				function()
+					Snacks.picker()
+				end,
+				{ desc = "[S]earch [S]elect Pickers" },
+			},
+			{
+				"n",
+				"<leader>sw",
+				function()
+					Snacks.picker.grep_word()
+				end,
+				{ desc = "[S]earch current [W]ord" },
+			},
+			{ "n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" } },
+			{ "n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" } },
+			{ "n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" } },
+			{ "n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat' } },
+		},
 	},
 }
