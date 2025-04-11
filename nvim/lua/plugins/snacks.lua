3ec6be2e9a13554aee66a51c440002b7b9516e43
🤖 automated <cron@job>
2025-04-11 08:46:51 +0200

    MacBook Pro Tim (2)

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -15,46 +15,136 @@ return {
 		statuscolumn = { enabled = true },
 		picker = {
 			enabled = true,
+			layout = { preset = "telescope" },
 		},
-		keys = {
-
-			{
-				"n",
-				"<leader>sh",
-				function()
-					Snacks.picker.help()
-				end,
-				{ desc = "[S]earch [H]elp" },
-			},
-			-- {"n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" }},
-			{
-				"n",
-				"<leader>sf",
-				function()
-					Snacks.picker.smart()
-				end,
-				{ desc = "[S]earch [F]iles" },
-			},
-			{
-				"n",
-				"<leader>ss",
-				function()
-					Snacks.picker()
-				end,
-				{ desc = "[S]earch [S]elect Pickers" },
-			},
-			{
-				"n",
-				"<leader>sw",
-				function()
-					Snacks.picker.grep_word()
-				end,
-				{ desc = "[S]earch current [W]ord" },
-			},
-			{ "n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" } },
-			{ "n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" } },
-			{ "n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" } },
-			{ "n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat' } },
+	},
+	keys = {
+		{
+			"<leader>sh",
+			function()
+				Snacks.picker.help()
+			end,
+			desc = "[S]earch [H]elp",
+		},
+		{
+			"<leader>sk",
+			function()
+				Snacks.picker.keymaps()
+			end,
+			desc = "[S]earch [K]eymaps",
+		},
+		{
+			"<leader>sf",
+			function()
+				Snacks.picker.files()
+			end,
+			desc = "[S]earch [F]iles",
+		},
+		{
+			"<leader>ss",
+			function()
+				Snacks.picker()
+			end,
+			desc = "[S]earch [S]elect Pickers",
+		},
+		{
+			"<leader>sw",
+			function()
+				Snacks.picker.grep_word()
+			end,
+			desc = "[S]earch current [W]ord",
+		},
+		{
+			"<leader>sg",
+			function()
+				Snacks.picker.grep()
+			end,
+			desc = "[S]earch by [G]rep",
+		},
+		{
+			"<leader>sd",
+			function()
+				Snacks.picker.diagnostics()
+			end,
+			desc = "[S]earch [D]iagnostics",
+		},
+		{
+			"<leader>sr",
+			function()
+				Snacks.picker.resume()
+			end,
+			desc = "[S]earch [R]esume",
+		},
+		{
+			"<leader>s.",
+			function()
+				Snacks.picker.recent()
+			end,
+			desc = '[S]earch Recent Files ("." for repeat',
+		},
+		{
+			"<leader><leader>",
+			function()
+				Snacks.picker.buffers()
+			end,
+			desc = "[ ] Find existing buffers",
+		},
+		{
+			"<leader>/",
+			function()
+				Snacks.picker.lines()
+			end,
+			desc = "[/] Fuzzily search in current buffer",
+		},
+		{
+			"<leader>s/",
+			function()
+				Snacks.picker.grep_buffers()
+			end,
+			desc = "[S]earch [/] in Open Files",
+		},
+		{
+			"<leader>sn",
+			function()
+				Snacks.picker.files { cwd = vim.fn.stdpath "config" }
+			end,
+			desc = "[S]earch [N]eovim files",
+		},
+		{
+			"gd",
+			function()
+				Snacks.picker.lsp_definitions()
+			end,
+			desc = "[G]oto [D]efinition",
+		},
+		{
+			"gD",
+			function()
+				Snacks.picker.lsp_declarations()
+			end,
+			desc = "[G]oto [D]eclaration",
+		},
+		{
+			"gr",
+			function()
+				Snacks.picker.lsp_references()
+			end,
+			nowait = true,
+			desc = "[G]oto [R]eferences",
+		},
+		{
+			"gI",
+			function()
+				Snacks.picker.lsp_implementations()
+			end,
+			desc = "[G]oto [I]mplementation",
+		},
+		{
+			"<leader>D",
+			function()
+				Snacks.picker.lsp_type_definitions()
+			end,
+			desc = "Type [D]efinition",
 		},
 	},
 }
