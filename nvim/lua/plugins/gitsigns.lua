da5a966dc95c19141394f74048981c7ef9adf298
🤖 automated <cron@job>
2025-04-28 11:28:24 +0200

    MacBook Pro Tim (1)

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -3,12 +3,12 @@ return {
 	-- See `:help gitsigns` to understand what the configuration keys do
 	"lewis6991/gitsigns.nvim",
 	opts = {
-		-- signs = {
-		-- 	add = { text = "+" },
-		-- 	change = { text = "~" },
-		-- 	delete = { text = "_" },
-		-- 	topdelete = { text = "‾" },
-		-- 	changedelete = { text = "~" },
-		-- },
+		signs = {
+			add = { text = "+" },
+			change = { text = "~" },
+			delete = { text = "_" },
+			topdelete = { text = "‾" },
+			changedelete = { text = "~" },
+		},
 	},
 }
