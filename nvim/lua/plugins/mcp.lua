5a36f3f786539877b88cab746793cea7afcf580d
🤖 automated <cron@job>
2025-04-30 13:51:27 +0200

    MacBook Tim (3)

--- nvim/lua/plugins/mcp.lua
+++ nvim/lua/plugins/mcp.lua
@@ -1,11 +1,11 @@
 return {
 	"ravitemer/mcphub.nvim",
 	dependencies = {
-		"nvim-lua/plenary.nvim",
+		"nvim-lua/plenary.nvim", -- Required for Job and HTTP requests
 	},
-	-- cmd = "MCPHub",
-	build = "npm install -g mcp-hub@latest",
-	opts = {
-		auto_approve = true,
-	},
+	--cmd = "MCPHub",  -- lazy load
+	build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
+	config = function()
+		require("mcphub").setup()
+	end,
 }
