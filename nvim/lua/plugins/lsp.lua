dfb5fd7ae93048d667c36256ba48a80e782a7e96
🤖 automated <cron@job>
2025-05-12 09:48:24 +0200

    MacBook Pro Tim (2)

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -17,9 +17,15 @@ return {
 		"neovim/nvim-lspconfig",
 		dependencies = {
 			-- Automatically install LSPs and related tools to stdpath for Neovim
-			{ "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
-			"williamboman/mason-lspconfig.nvim",
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
+			{
+				"williamboman/mason.nvim",
+				config = true,
+			},
+			{
+				"williamboman/mason-lspconfig.nvim",
+			},
+
+			{ "WhoIsSethDaniel/mason-tool-installer.nvim" },
 
 			-- Useful status updates for LSP.
 			{ "j-hui/fidget.nvim", opts = {} },
@@ -335,7 +341,7 @@ return {
 				-- 		},
 				-- 	},
 				-- },
-				ruff = {},
+				-- ruff = {},
 				basedpyright = {
 					settings = {
 						basedpyright = {
