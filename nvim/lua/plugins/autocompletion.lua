a3884a424e90e9efdb362840c9afe1626a9d2660
🤖 automated <cron@job>
2025-05-15 08:53:56 +0200

    MacBook Pro Tim (2)

--- nvim/lua/plugins/autocompletion.lua
+++ nvim/lua/plugins/autocompletion.lua
@@ -13,9 +13,9 @@ return {
 			"mikavilpas/blink-ripgrep.nvim",
 		},
 
-		commit = "6dc82a0",
+		version = "*",
 		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-		build = "cargo build --release",
+		-- build = "cargo build --release",
 		-- If you use nix, you can build from source using latest nightly rust with:
 		-- build = 'nix run .#build-plugin',
 
