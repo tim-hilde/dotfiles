77a5dd40f6f2b02cf9ec74d0d28229616d573864
🤖 automated <cron@job>
2025-06-07 21:12:25 +0200

    MacBook Tim (3)

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -6,7 +6,21 @@ return {
 		main = "nvim-treesitter.configs", -- Sets main module to use for opts
 		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 		opts = {
-			ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "markdown_inline", "query", "vim", "vimdoc", "python" },
+			ensure_installed = {
+				"bash",
+				"c",
+				"diff",
+				"html",
+				"lua",
+				"luadoc",
+				"markdown",
+				"markdown_inline",
+				"query",
+				"vim",
+				"vimdoc",
+				"python",
+				"yaml",
+			},
 			-- Autoinstall languages that are not installed
 			auto_install = true,
 			highlight = {
