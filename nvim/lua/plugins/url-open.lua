b88767e07eab51be3292590f8138e0ffe3a699fd
🤖 automated <cron@job>
2025-06-09 15:25:53 +0200

    MacBook Tim (1)

--- nvim/lua/plugins/url-open.lua
+++ nvim/lua/plugins/url-open.lua
@@ -3,10 +3,6 @@ return {
 	event = "VeryLazy",
 	cmd = "URLOpenUnderCursor",
 	config = function()
-		local status_ok, url_open = pcall(require, "url-open")
-		if not status_ok then
-			return
-		end
-		url_open.setup {}
+		require "url-open"
 	end,
 }
