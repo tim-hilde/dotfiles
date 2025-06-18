0c6a8883501045d89c7b95c274e2bcfc01fbfda8
🤖 automated <cron@job>
2025-06-18 15:25:50 +0200

    MacBook Pro Tim (1)

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -5,12 +5,6 @@
 --  See `:help hlsearch`
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
--- TIP: Disable arrow keys in normal mode
-vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
-
 -- HJKL as amplified versions of hjkl
 vim.keymap.set("n", "J", "6j")
 vim.keymap.set("n", "K", "6k")
