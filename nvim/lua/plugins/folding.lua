0bbc330bdc59fe3ebea57b5fff33074e734694d8
🤖 automated <cron@job>
2025-08-07 08:15:23 +0200

    MacBook Pro Tim (1)

--- nvim/lua/plugins/folding.lua
+++ nvim/lua/plugins/folding.lua
@@ -1,46 +1,24 @@
--- -- Nice and simple folding:
--- vim.o.foldenable = true
--- vim.opt.foldlevel = 99
--- vim.opt.foldlevelstart = 99
--- vim.o.foldmethod = "expr"
--- vim.o.foldtext = ""
--- vim.opt.foldcolumn = "0"
--- vim.opt.fillchars:append { fold = " " }
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
 
--- -- Default to treesitter folding
--- vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
--- -- Prefer LSP folding if client supports it
--- vim.api.nvim_create_autocmd("LspAttach", {
--- 	callback = function(args)
--- 		local client = vim.lsp.get_client_by_id(args.data.client_id)
--- 		if client and client:supports_method "textDocument/foldingRange" then
--- 			local win = vim.api.nvim_get_current_win()
--- 			vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
--- 		end
--- 	end,
--- })
 return {
-	-- {
-	-- 	"chrisgrieser/nvim-origami",
-	-- 	event = "VeryLazy",
-	-- 	opts = {
-	-- 		pauseFoldsOnSearch = true,
-	-- 		foldtext = {
-	-- 			enabled = true,
-	-- 			template = "   %s lines", -- `%s` gets the number of folded lines
-	-- 		},
-	-- 		foldKeymaps = {
-	-- 			setup = true, -- modifies `h` and `l`
-	-- 			hOnlyOpensOnFirstColumn = true,
-	-- 		},
-	-- 		autoFold = {
-	-- 			enabled = false,
-	-- 			kinds = { "comment", "imports" }, ---@type lsp.FoldingRangeKind[]
-	-- 		},
-	-- 	},
-	-- 	init = function()
-	-- 		vim.opt.foldlevel = 99
-	-- 		vim.opt.foldlevelstart = 99
-	-- 	end,
-	-- },
+	{
+		"chrisgrieser/nvim-origami",
+		event = "VeryLazy",
+		opts = {
+			pauseFoldsOnSearch = true,
+			foldtext = {
+				enabled = true,
+				template = "   %s lines", -- `%s` gets the number of folded lines
+			},
+			foldKeymaps = {
+				setup = true, -- modifies `h` and `l`
+				hOnlyOpensOnFirstColumn = true,
+			},
+			autoFold = {
+				enabled = false,
+				kinds = { "comment", "imports" }, ---@type lsp.FoldingRangeKind[]
+			},
+		},
+	},
 }
