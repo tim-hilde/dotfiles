d26cdbd1766a5464c26ce760183ebe3a0a43ab9b
🤖 automated <cron@job>
2025-08-22 14:43:45 +0200

    MacBook Pro Tim (1)

--- nvim/lua/plugins/iron.lua
+++ nvim/lua/plugins/iron.lua
@@ -9,14 +9,15 @@ return {
 				-- Whether a repl should be discarded or not
 				scratch_repl = true,
 				repl_definition = {
-					python = {
-						command = { "jupyter-console", "--ZMQTerminalInteractiveShell.image_handler=None" },
-						format = require("iron.fts.common").bracketed_paste_python,
-						block_dividers = { "# %%", "#%%" },
-					}, -- python = {
-					-- 	command = { "ipython", "--no-autoindent" },
-					-- 	format = python_format,
+					-- python = {
+					-- 	command = { "jupyter-console", "--ZMQTerminalInteractiveShell.image_handler=None" },
+					-- 	format = require("iron.fts.common").bracketed_paste_python,
+					-- 	block_dividers = { "# %%", "#%%" },
 					-- },
+					python = {
+						command = { "ipython", "--no-autoindent" },
+						format = python_format,
+					},
 				},
 				-- How the repl window will be displayed
 				repl_open_cmd = view.split.vertical.botright(0.5, {
