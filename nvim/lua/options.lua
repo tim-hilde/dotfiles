4c29e458c04cd6e7143c6bce3366e30eb56df0de
🤖 automated <cron@job>
2025-08-25 14:55:43 +0200

    MacBook Pro Tim (3)

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -52,8 +52,10 @@ vim.opt.scrolloff = 20
 
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
+vim.opt.softtabstop = 0
 vim.opt.shiftwidth = 4
+
+vim.g.python_recommended_style = 0
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = "a"
--- nvim/lua/plugins/vim-sleuth.lua
+++ nvim/lua/plugins/vim-sleuth.lua
@@ -1,3 +1,3 @@
 return {
-	-- "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 }
