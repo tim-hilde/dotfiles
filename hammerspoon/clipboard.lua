b746788377a6ffc6d0e73fc8c1c736da73dabfaf
🤖 automated <cron@job>
2025-09-12 10:39:04 +0200

    MacBook Pro Tim (1)

---
+++ hammerspoon/clipboard.lua
@@ -0,0 +1,30 @@
+local config = require("config")
+
+local clipboard = {}
+
+-- Double-tap Cmd+C state
+local lastCmdCTime = 0
+
+-- Double-tap Cmd+C to merge clipboard content
+hs.hotkey.bind({ "cmd" }, "c", function()
+	local currentTime = hs.timer.secondsSinceEpoch()
+	local timeSinceLastC = currentTime - lastCmdCTime
+
+	if timeSinceLastC <= config.doubleTapThreshold then
+		-- Double-tap detected - merge text
+		hs.eventtap.keyStroke({ "cmd" }, "c") -- Copy selected text
+		hs.timer.doAfter(0.1, function()
+			local currentText = hs.pasteboard.getContents()
+			local previousText = hs.pasteboard.getHistory()[2] or ""
+			local mergedText = previousText .. "\n" .. currentText
+			hs.pasteboard.setContents(mergedText)
+		end)
+	else
+		-- Normal Cmd+C
+		hs.eventtap.keyStroke({ "cmd" }, "c")
+	end
+
+	lastCmdCTime = currentTime
+end)
+
+return clipboard
