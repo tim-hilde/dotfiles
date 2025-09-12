ea599a87c64ba5a039e8e3b3e11b8bd1144533cd
🤖 automated <cron@job>
2025-09-12 10:39:44 +0200

    MacBook Pro Tim (1)

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,40 +1,12 @@
-local config = {
-	syncIntervalMins = 30,
-}
+local config = require("config")
+local utils = require("utils")
 
-local function notify(msg)
-	hs.notify.show("Hammerspoon", "", msg)
-end
+-- Load modules
+require("dotfiles")
+require("clipboard")
 
+-- Initialize reload configuration
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
--- Save brew file
-local function runBrewBundleDump()
-	hs.execute("brew bundle dump --force --file='~/dotfiles/brew/Brewfile'")
-end
-
--- Sync dotfiles
-local function syncDotfiles()
-	local syncScriptPath = "~/dotfiles/sync-this-repo.sh"
-	hs.task
-		.new(syncScriptPath, function(exitCode, stdout, stderr)
-			if exitCode ~= 0 then
-				local output = (stdout .. "\n" .. stderr):gsub("^%s+", ""):gsub("%s+$", "")
-				local msg = ("⚠️️ %s"):format(output)
-				print(msg)
-				hs.alert(msg, 5)
-				notify(msg)
-			end
-		end)
-		:start()
-end
-syncDotfiles()
-hs.timer
-	.doEvery(config.syncIntervalMins * 30, function()
-		local idleMins = hs.host.idleTime() / 30
-		if idleMins < config.syncIntervalMins then
-			syncDotfiles()
-		end
-	end)
-	:start()
+utils.notify("Hammerspoon configuration loaded")
