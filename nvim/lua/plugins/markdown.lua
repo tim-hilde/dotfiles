ca10a1a98cb5cf5db558bf4e819392fcc2c28b70
🤖 automated <cron@job>
2025-09-12 15:21:09 +0200

    MacBook Pro Tim (2)

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -11,7 +11,8 @@ return {
 		init = function()
 			-- vim.cmd [[set foldlevelstart=6]]
 			vim.cmd [[set conceallevel=2]]
-			vim.cmd [[colorscheme catppuccin]]
+			-- vim.cmd [[colorscheme catppuccin]]
+
 			require("markview").setup { markdown = {
 				list_items = {
 					shift_width = 2,
