6568458e410cd80d2c4b7b76e446c317fdfcee5e
🤖 automated <cron@job>
2025-09-15 17:05:30 +0200

    MacBook Pro Tim (2)

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -9,17 +9,9 @@ return {
 				noice = true,
 				notify = true,
 			},
-			transparent_background = true, -- disables setting the background color.
+			transparent_background = false, -- disables setting the background color.
 			float = {
-				transparent = true, -- enable transparent floating windows
-				solid = false, -- use solid styling for floating windows, see |winborder|
-			},
-			show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-			term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-			dim_inactive = {
-				enabled = false, -- dims the background color of inactive window
-				shade = "dark",
-				percentage = 0.15, -- percentage of the shade to apply to the inactive window
+				transparent = false, -- enable transparent floating windows
 			},
 		}
 		vim.cmd.colorscheme "catppuccin-mocha"
