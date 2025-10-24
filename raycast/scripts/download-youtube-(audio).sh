f998357327ecf4f6dddb9fc8abc16df6609cf78e
🤖 automated <cron@job>
2025-10-24 19:21:10 +0200

    MacBook Tim (2)

--- raycast/scripts/download-youtube-(audio).sh
+++ raycast/scripts/download-youtube-(audio).sh
@@ -3,7 +3,7 @@
 # Required parameters:
 # @raycast.schemaVersion 1
 # @raycast.title Download Youtube (Audio)
-# @raycast.mode compact
+# @raycast.mode fullOutput
 # @raycast.argument1 {"type": "text", "placeholder": "url", "optional": false}
 
 # Optional parameters:
@@ -14,8 +14,4 @@
 
 url="${1}"
 
-if yt-dlp -f bestaudio --extract-audio --audio-format mp3 "${url}"; then
-    echo "✅ Audio download completed"
-else
-    echo "❌ Audio download failed"
-fi
+yt-dlp -f bestaudio --extract-audio --audio-format mp3 "${url}"
