bb810ac271e3e07822c14015e3221b125eb53e7c
🤖 automated <cron@job>
2025-11-08 18:30:59 +0100

    MacBook Tim (2)

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -26,16 +26,4 @@ return {
 			}
 		end,
 	},
-	-- {
-	-- 	"preservim/vim-markdown",
-	-- 	ft = { "markdown", "codecompanion" },
-	-- 	init = function()
-	-- 		vim.cmd [[set foldlevelstart=6]]
-	-- 		vim.cmd [[set conceallevel=2]]
-	-- 		vim.cmd [[let g:vim_markdown_auto_insert_bullets = 0]]
-	-- 	end,
-	-- 	dependencies = {
-	-- 		"godlygeek/tabular",
-	-- 	},
-	-- },
 }
