95de6fecee2f7a71b2de7fad66e3e70672a2ea22
🤖 automated <cron@job>
2025-11-08 16:10:48 +0100

    MacBook Tim (1)

--- nvim/lua/plugins/code-companion.lua
+++ nvim/lua/plugins/code-companion.lua
@@ -126,14 +126,60 @@ return {
 				},
 				acp = {
 					opencode = function()
-						return require("codecompanion.adapters").extend("opencode", {
+						---@class CodeCompanion.ACPAdapter.OpenCode: CodeCompanion.ACPAdapter
+						return {
+							name = "opencode",
+							formatted_name = "OpenCode",
+							type = "acp",
+							roles = {
+								llm = "assistant",
+								user = "user",
+							},
+							opts = {
+								vision = true,
+							},
 							commands = {
 								default = {
 									"opencode",
 									"acp",
 								},
 							},
-						})
+							defaults = {
+								mcpServers = {},
+								timeout = 20000, -- 20 seconds
+							},
+							parameters = {
+								protocolVersion = 1,
+								clientCapabilities = {
+									fs = { readTextFile = true, writeTextFile = true },
+								},
+								clientInfo = {
+									name = "CodeCompanion.nvim",
+									version = "1.0.0",
+								},
+							},
+							handlers = {
+								---@param self CodeCompanion.ACPAdapter
+								---@return boolean
+								setup = function(self)
+									return true
+								end,
+
+								---@param self CodeCompanion.ACPAdapter
+								---@param messages table
+								---@param capabilities table
+								---@return table
+								form_messages = function(self, messages, capabilities)
+									return helpers.form_messages(self, messages, capabilities)
+								end,
+
+								---Function to run when the request has completed. Useful to catch errors
+								---@param self CodeCompanion.ACPAdapter
+								---@param code number
+								---@return nil
+								on_exit = function(self, code) end,
+							},
+						}
 					end,
 				},
 			},
