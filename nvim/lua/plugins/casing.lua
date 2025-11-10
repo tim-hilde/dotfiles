ccc478b8e3cad4f1c40fff6e59afe0630eaf49c8
🤖 automated <cron@job>
2025-11-10 10:58:01 +0100

    MacBook Pro Tim (1)

--- nvim/lua/plugins/casing.lua
+++ nvim/lua/plugins/casing.lua
@@ -1,3 +1,10 @@
 return {
 	"johmsalas/text-case.nvim",
+	opts = {
+		default_keymappings_enabled = true,
+		-- `prefix` is only considered if `default_keymappings_enabled` is true. It configures the prefix
+		-- of the keymappings, e.g. `gau ` executes the `current_word` method with `to_upper_case`
+		-- and `gaou` executes the `operator` method with `to_upper_case`.
+		prefix = "g.",
+	},
 }
