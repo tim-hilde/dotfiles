06331f882929945d26c8621902602c0dc77fe267
🤖 automated <cron@job>
2025-11-24 20:04:10 +0100

    MacBook Pro Tim (2)

--- nvim/lua/plugins/code-companion.lua
+++ nvim/lua/plugins/code-companion.lua
@@ -275,7 +275,7 @@ return {
 							---Adapter for generating titles (defaults to current chat adapter)
 							adapter = "copilot", -- "copilot"
 							---Model for generating titles (defaults to current chat model)
-							model = nil, -- "gpt-4o"
+							model = "gpt-4o", -- "gpt-4o"
 							---Number of user prompts after which to refresh the title (0 to disable)
 							refresh_every_n_prompts = 0, -- e.g., 3 to refresh after every 3rd user prompt
 							---Maximum number of times to refresh the title (default: 3)
