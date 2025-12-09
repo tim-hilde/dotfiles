475ae8527ac14d46b9af763a599ad48479db02d1
🤖 automated <cron@job>
2025-12-09 22:10:03 +0100

    MacBook Tim (2)

--- nvim/lua/plugins/presentation.lua
+++ nvim/lua/plugins/presentation.lua
@@ -1,19 +1,6 @@
 return {
 	{
-		"sotte/presenting.nvim",
-		opts = {
-			-- fill in your options here
-			-- see :help Presenting.config
-		},
-		cmd = { "Presenting" },
-	},
-
-	{
-		"ducks/vimdeck.nvim",
-		cmd = { "Vimdeck", "VimdeckFile" },
-		opts = {
-			use_figlet = false,
-			center_slides = false,
-		},
+		"tjdevries/present.nvim",
+		opts = { syntax = { stop = "---" } },
 	},
 }
