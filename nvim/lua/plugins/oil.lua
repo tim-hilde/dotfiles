903c8d92a935748f0862fc1913c0393eb0714af8
🤖 automated <cron@job>
2026-02-02 13:55:38 +0100

    MacBook Pro Tim (1)

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,15 +1,3 @@
--- Declare a global function to retrieve the current directory
-function _G.get_oil_winbar()
-	local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
-	local dir = require("oil").get_current_dir(bufnr)
-	if dir then
-		return vim.fn.fnamemodify(dir, ":~")
-	else
-		-- If there is no current directory (e.g. over ssh), just show the buffer name
-		return vim.api.nvim_buf_get_name(0)
-	end
-end
-
 return {
 	"stevearc/oil.nvim",
 	---@module 'oil'
@@ -24,11 +12,8 @@ return {
 			["<C-v>"] = { "actions.select", opts = { vertical = true } },
 			["<C-h>"] = { "actions.select", opts = { horizontal = true } },
 		},
-		win_options = {
-			winbar = "%!v:lua.get_oil_winbar()",
-		},
 		float = {
-			border = 10,
+			border = "rounded",
 		},
 	},
 	-- Optional dependencies
