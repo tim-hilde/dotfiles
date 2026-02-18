da7e561689f3d858869f1fc8448bf150a1b4d001
🤖 automated <cron@job>
2026-02-18 16:56:33 +0100

    MacBook Pro Tim (1)

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -26,11 +26,4 @@ return {
 			}
 		end,
 	},
-	{
-		"3rd/diagram.nvim",
-		dependencies = {
-			{ "3rd/image.nvim", opts = {} }, -- you'd probably want to configure image.nvim manually instead of doing this
-		},
-		opts = {},
-	},
 }
