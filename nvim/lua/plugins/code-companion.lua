8df92de74b22da289ca8fd6dfe3c13923237464d
🤖 automated <cron@job>
2026-03-13 20:03:20 +0100

    MacBook Tim (2)

--- nvim/lua/plugins/code-companion.lua
+++ nvim/lua/plugins/code-companion.lua
@@ -245,7 +245,7 @@ return {
 					end,
 					opts = {
 						show_result_in_chat = true, -- Show the mcp tool result in the chat buffer
-						make_vars = false, -- make chat #variables from MCP server resources
+						make_vars = true, -- make chat #variables from MCP server resources
 						make_slash_commands = true, -- make /slash_commands from MCP server prompts
 					},
 				},
