2bb15725841fcce5b2a08dd2eb50ca9710a27386
🤖 automated <cron@job>
2026-03-26 16:40:57 +0100

    MacBook Pro Tim (1)

--- nvim/lua/plugins/code-companion.lua
+++ nvim/lua/plugins/code-companion.lua
@@ -96,6 +96,15 @@ return {
 			},
 
 			adapters = {
+				acp = {
+					claude_code = function()
+						return require("codecompanion.adapters").extend("claude_code", {
+							env = {
+								CLAUDE_CODE_OAUTH_TOKEN = "CLAUDE_CODE_OAUTH_TOKEN",
+							},
+						})
+					end,
+				},
 				http = {
 					copilot = function()
 						return require("codecompanion.adapters").extend("copilot", {
