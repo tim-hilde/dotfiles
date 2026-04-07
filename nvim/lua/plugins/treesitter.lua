0fb2ca4039378be2bfa9835992034b3985292154
🤖 automated <cron@job>
2026-04-07 13:03:29 +0200

    MacBook Pro Tim (1)

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -28,6 +28,8 @@ return {
 			local indent_disabled = { ruby = true }
 			local regex_highlight = { ruby = true }
 
+			vim.treesitter.language.register("bash", "env")
+
 			vim.api.nvim_create_autocmd("FileType", {
 				group = vim.api.nvim_create_augroup("treesitter_setup", { clear = true }),
 				desc = "Treesitter highlight and indent",
@@ -69,19 +71,20 @@ return {
 			-- SELECT (lazy require wie in der README)
 			-------------------------------------------------
 			local select_maps = {
-				["af"] = "@function.outer",
-				["if"] = "@function.inner",
-				["ac"] = "@class.outer",
-				["ic"] = "@class.inner",
-				["aa"] = "@parameter.outer",
-				["ia"] = "@parameter.inner",
-				["ar"] = "@assignment.rhs",
-				["al"] = "@assignment.lhs",
+				{ "af", "@function.outer", "Outer function" },
+				{ "if", "@function.inner", "Inner function" },
+				{ "ac", "@class.outer", "Outer class" },
+				{ "ic", "@class.inner", "Inner class" },
+				{ "aa", "@parameter.outer", "Outer parameter" },
+				{ "ia", "@parameter.inner", "Inner parameter" },
+				{ "ar", "@assignment.rhs", "Right side assignment" },
+				{ "al", "@assignment.lhs", "Left side assignment" },
 			}
-			for key, query in pairs(select_maps) do
+			for _, m in ipairs(select_maps) do
+				local key, query, desc = m[1], m[2], m[3]
 				vim.keymap.set({ "x", "o" }, key, function()
 					require("nvim-treesitter-textobjects.select").select_textobject(query, "textobjects")
-				end, { desc = "TS: " .. query })
+				end, { desc = desc })
 			end
 
 			-------------------------------------------------
@@ -89,41 +92,41 @@ return {
 			-------------------------------------------------
 			local move_maps = {
 				-- goto_next_start
-				{ "]f", "goto_next_start", "@function.outer", "textobjects" },
-				{ "]c", "goto_next_start", "@class.outer", "textobjects" },
-				{ "]a", "goto_next_start", "@parameter.outer", "textobjects" },
-				{ "]r", "goto_next_start", "@assignment.rhs", "textobjects" },
-				{ "]z", "goto_next_start", "@fold", "folds" },
+				{ "]f", "goto_next_start", "@function.outer", "textobjects", "Next function start" },
+				{ "]c", "goto_next_start", "@class.outer", "textobjects", "Next class start" },
+				{ "]a", "goto_next_start", "@parameter.outer", "textobjects", "Next parameter start" },
+				{ "]r", "goto_next_start", "@assignment.rhs", "textobjects", "Next right side assignment start" },
+				{ "]z", "goto_next_start", "@fold", "folds", "Next fold" },
 				-- goto_next_end
-				{ "]F", "goto_next_end", "@function.outer", "textobjects" },
-				{ "]C", "goto_next_end", "@class.outer", "textobjects" },
-				{ "]A", "goto_next_end", "@parameter.outer", "textobjects" },
+				{ "]F", "goto_next_end", "@function.outer", "textobjects", "Next function end" },
+				{ "]C", "goto_next_end", "@class.outer", "textobjects", "Next class end" },
+				{ "]A", "goto_next_end", "@parameter.outer", "textobjects", "Next parameter end" },
 				-- goto_previous_start
-				{ "[f", "goto_previous_start", "@function.outer", "textobjects" },
-				{ "[c", "goto_previous_start", "@class.outer", "textobjects" },
-				{ "[a", "goto_previous_start", "@parameter.outer", "textobjects" },
+				{ "[f", "goto_previous_start", "@function.outer", "textobjects", "Previous function start" },
+				{ "[c", "goto_previous_start", "@class.outer", "textobjects", "Previous class start" },
+				{ "[a", "goto_previous_start", "@parameter.outer", "textobjects", "Previous parameter start" },
 				-- goto_previous_end
-				{ "[F", "goto_previous_end", "@function.outer", "textobjects" },
-				{ "[C", "goto_previous_end", "@class.outer", "textobjects" },
-				{ "[A", "goto_previous_end", "@parameter.outer", "textobjects" },
+				{ "[F", "goto_previous_end", "@function.outer", "textobjects", "Previous function end" },
+				{ "[C", "goto_previous_end", "@class.outer", "textobjects", "Previous class end" },
+				{ "[A", "goto_previous_end", "@parameter.outer", "textobjects", "Previous parameter end" },
 			}
 			for _, m in ipairs(move_maps) do
-				local key, fn_name, query, group = m[1], m[2], m[3], m[4]
+				local key, fn_name, query, group, desc = m[1], m[2], m[3], m[4], m[5]
 				vim.keymap.set({ "n", "x", "o" }, key, function()
 					require("nvim-treesitter-textobjects.move")[fn_name](query, group)
-				end, { desc = "TS move: " .. key .. " " .. query })
+				end, { desc = desc })
 			end
 
 			-------------------------------------------------
 			-- REPEATABLE MOVES mit ; und ,
 			-------------------------------------------------
-			-- local ts_repeat = require "nvim-treesitter-textobjects.repeatable_move"
-			-- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat.repeat_last_move_next)
-			-- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat.repeat_last_move_previous)
-			-- vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat.builtin_f_expr, { expr = true })
-			-- vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat.builtin_F_expr, { expr = true })
-			-- vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat.builtin_t_expr, { expr = true })
-			-- vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat.builtin_T_expr, { expr = true })
+			local ts_repeat = require "nvim-treesitter-textobjects.repeatable_move"
+			vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat.repeat_last_move_next)
+			vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat.repeat_last_move_previous)
+			vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat.builtin_f_expr, { expr = true })
+			vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat.builtin_F_expr, { expr = true })
+			vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat.builtin_t_expr, { expr = true })
+			vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat.builtin_T_expr, { expr = true })
 		end,
 	},
 	{
