34504038cd9fd3a34cfce3e3b95f5c954a38fe68
🤖 automated <cron@job>
2026-06-23 13:39:22 +0200

    MacBook Merlin (1)

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -2,6 +2,7 @@ return {
 	-- Adds git related signs to the gutter, as well as utilities for managing changes
 	-- See `:help gitsigns` to understand what the configuration keys do
 	"lewis6991/gitsigns.nvim",
+	commit = "25050e4ed39e628282831d4cbecb1850454ce915",
 	opts = {
 		signs = {
 			add = { text = "+" },
