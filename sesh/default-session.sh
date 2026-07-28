2861d4f19c36930ad76123ecc3ab027247a34086
🤖 automated <cron@job>
2026-07-28 14:36:21 +0200

    MacBook Merlin (1)

--- sesh/default-session.sh
+++ sesh/default-session.sh
@@ -1,6 +1,8 @@
 tmux send-keys "nvim" Enter
 tmux new-window
-tmux new-window 
+tmux new-window
 tmux rename-window "lazygit"
 tmux send-keys "lazygit" Enter
+tmux new-window
+tmux send-keys "opencode" Enter
 tmux select-window -t 1
